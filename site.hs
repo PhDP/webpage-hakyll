6224022384235aaf7a731d255524559c0605a712
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2013-05-21 18:36:38 -0400

    Posts + update

--- site.hs
+++ site.hs
@@ -1,44 +1,51 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+import Data.Monoid (mappend)
+import Hakyll
+import GHC.IO.Encoding
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route idRoute
-        compile copyFileCompiler
+main = do
+    setLocaleEncoding utf8
+    setFileSystemEncoding utf8
+    setForeignEncoding utf8
+    hakyll $ do 
+		match "files/*" $ do
+			route idRoute
+			compile copyFileCompiler
 
-    match "css/*" $ do
-        route idRoute
-        compile compressCssCompiler
-
-    match "posts/*.html" $ do
-        route idRoute
-        compile $ do
-            getResourceBody
-                >>= loadAndApplyTemplate "templates/post.html" postCtx
-                >>= relativizeUrls
-
-    match "*.html" $ do
-        route idRoute
-        compile $ do
-            let indexCtx = field "posts" $ \_ -> postList $ fmap (take 3) . recentFirst
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateCompiler
-
+		match "images/*" $ do
+			route idRoute
+			compile copyFileCompiler
+	
+		match "css/*" $ do
+			route idRoute
+			compile compressCssCompiler
+	
+		match "posts/*.html" $ do
+			route idRoute
+			compile $ do
+				getResourceBody
+					>>= loadAndApplyTemplate "templates/post.html" postCtx
+					>>= relativizeUrls
+	
+		match "*.html" $ do
+			route idRoute
+			compile $ do
+				let indexCtx = field "posts" $ \_ -> postList $ fmap (take 30) . recentFirst
+				getResourceBody
+					>>= applyAsTemplate indexCtx
+					>>= loadAndApplyTemplate "templates/default.html" postCtx
+					>>= relativizeUrls
+	
+		match "templates/*" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
-
 
 --------------------------------------------------------------------------------
 postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
