7feee3c5e2e03b705b69c21d55d444b21ccf272a
Philippe Desjardins-Proulx <phdp@outlook.com>
2014-01-15 00:33:30 -0500

    comments

--- site.hs
+++ site.hs
@@ -1,76 +1,85 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import Data.Monoid (mappend)
 import Hakyll
 import GHC.IO.Encoding
 
---------------------------------------------------------------------------------
 main :: IO ()
-main = do
-    setLocaleEncoding utf8
-    setFileSystemEncoding utf8
-    setForeignEncoding utf8
-    hakyll $ do 
-        match "files/*" $ do
-        route idRoute
-        compile copyFileCompiler
-
-        match "images/*" $ do
+main =
+    do
+        setLocaleEncoding utf8
+        setFileSystemEncoding utf8
+        setForeignEncoding utf8
+        hakyll $ do 
+            -- Get files
+            match "files/*" $ do
             route idRoute
             compile copyFileCompiler
-    
-        match "css/*" $ do
-            route idRoute
-            compile compressCssCompiler
-    
-        match "posts/*.html" $ do
-            route idRoute
-            compile $ do
-                getResourceBody
-                    >>= loadAndApplyTemplate "templates/post.html" postCtx
-                    >>= saveSnapshot "content"
-                    >>= loadAndApplyTemplate "templates/default.html" postCtx
-                    >>= relativizeUrls
-    
-        match "*.html" $ do
-            route idRoute
-            compile $ do
-                let indexCtx = field "posts" $ \_ -> postList $ fmap (take 30) . recentFirst
-                getResourceBody
-                    >>= applyAsTemplate indexCtx
-                    >>= loadAndApplyTemplate "templates/default.html" postCtx
-                    >>= relativizeUrls
-    
-        match "templates/*" $ compile templateCompiler
+        
+            -- Get images
+            match "images/*" $ do
+                route idRoute
+                compile copyFileCompiler
+        
+            -- Compile & compress CSS
+            match "css/*" $ do
+                route idRoute
+                compile compressCssCompiler
+        
+            -- Get posts
+            match "posts/*.html" $ do
+                route idRoute
+                compile $ do
+                    getResourceBody
+                        >>= loadAndApplyTemplate "templates/post.html" postCtx
+                        >>= saveSnapshot "content"
+                        >>= loadAndApplyTemplate "templates/default.html" postCtx
+                        >>= relativizeUrls
+        
+            -- Parse html files
+            match "*.html" $ do
+                route idRoute
+                compile $ do
+                    let indexCtx = field "posts" $ \_ -> postList $ fmap (take 30) . recentFirst
+                    getResourceBody
+                        >>= applyAsTemplate indexCtx
+                        >>= loadAndApplyTemplate "templates/default.html" postCtx
+                        >>= relativizeUrls
+        
+            -- Compile templates
+            match "templates/*" $ compile templateCompiler
+        
+            -- Built the atom.xml file
+            create ["atom.xml"] $ do
+                route idRoute
+                compile $ do
+                    let feedCtx = postCtx `mappend` bodyField "description"
+                    posts <- fmap (take 10) . recentFirst =<<
+                        loadAllSnapshots "posts/*" "content"
+                    renderAtom myFeedConfiguration feedCtx posts
 
-        create ["atom.xml"] $ do
-            route idRoute
-            compile $ do
-                let feedCtx = postCtx `mappend` bodyField "description"
-                posts <- fmap (take 10) . recentFirst =<<
-                    loadAllSnapshots "posts/*" "content"
-                renderAtom myFeedConfiguration feedCtx posts
-
---------------------------------------------------------------------------------
+-- Format for posts
 postCtx :: Context String
 postCtx =
     dateField "date" "%Y.%m.%d" `mappend`
     defaultContext
 
---------------------------------------------------------------------------------
+-- Build the list of posts
 postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
-postList sortFilter = do
-    posts   <- sortFilter =<< loadAll "posts/*"
-    itemTpl <- loadBody "templates/post-item.html"
-    list    <- applyTemplateList itemTpl postCtx posts
-    return list
+postList sortFilter =
+    do
+        posts   <- sortFilter =<< loadAll "posts/*"
+        itemTpl <- loadBody "templates/post-item.html"
+        list    <- applyTemplateList itemTpl postCtx posts
+        return list
 
+-- Config for the Atom.xml file.
 myFeedConfiguration :: FeedConfiguration
 myFeedConfiguration = FeedConfiguration
-    { feedTitle       = "Philippe Desjardins-Proulx"
-    , feedDescription = "Artificial Intelligence, Machine Learning, Programming, et al..."
-    , feedAuthorName  = "Philippe Desjardins-Proulx"
-    , feedAuthorEmail = "phdp@outlook.com"
-    , feedRoot        = "http://phdp.github.io/"
+    {
+        feedTitle       = "Philippe Desjardins-Proulx -- Posts",
+        feedDescription = "Artificial Intelligence, Machine Learning, Programming, Technology, et cetera...",
+        feedAuthorName  = "Philippe Desjardins-Proulx",
+        feedAuthorEmail = "phdp@outlook.com",
+        feedRoot        = "http://phdp.github.io/"
     }
 
