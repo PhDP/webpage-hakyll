1989978a0c84d724abdd0af5e75437047f3b10da
Philippe Desjardins-Proulx <philippe.desjardins.proulx@umontreal.ca>
2022-08-29 04:40:08 -0400

    put all js in one file, simplify css

--- js/my-js.js
+++ js/my-js.js
@@ -7,13 +7,21 @@ function setDarkMode(dark, preference) {
         document.documentElement.classList.remove('dark');
     }
 };
+
 const preference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
 if (localStorage.getItem('theme') === "dark" || (!('theme' in localStorage) && preference === 'dark')) {
     setDarkMode(true, preference);
 }
+
 window.onload = function () {
     document.getElementById('button-dark-mode').addEventListener('click', function() {
         setDarkMode(!document.documentElement.classList.contains('dark'), preference);
     });
 };
+    
+function toggleMenu() {
+  const x = document.getElementById('vertical-menu');
+  if (x.style.display === 'block') { x.style.display = 'none'; } else { x.style.display = 'block'; }
+}
 
